3cb3e4fa5c2c6c8ec8bf942549908c6986ff1c76
Sahil <sahildd28@gmail.com>
2024-11-20 01:38:55 +0530

    Updates

---
+++ _next/static/FX_Kr-BCJI0bvdWM21HHn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-c394631b58bced91b40c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fa39c7ce12d3ae2cec40.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
