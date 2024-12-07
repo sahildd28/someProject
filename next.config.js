d7f9efe4b3fdba02cc1c60aedef5420a2f12464a
Sahil <sahildd28@gmail.com>
2024-12-07 20:53:56 +0530

    changes done for next js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const isProd = process.env.NODE_ENV === 'production';
 
+// module.exports = {
+//   basePath: isProd ? '/someProject' : '',
+//   assetPrefix: isProd ? '/someProject/' : '',
+// };
 module.exports = {
-  basePath: isProd ? '/someProject' : '',
-  assetPrefix: isProd ? '/someProject/' : '',
+  basePath:'',
+  assetPrefix: '',
 };
